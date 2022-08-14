1e35e36ca2ab945ea13a4c25890fd533c5b59c8f
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-15 08:19:21 +1000

    Update join.js

--- static/join.js
+++ static/join.js
@@ -1,4 +1,4 @@
-function attempt_join(){
+function join(){
   console.log("joining game");
   var name = document.getElementById("f_name").value + " " + document.getElementById("l_name").value;
   var code = document.getElementById("game_code").value;
