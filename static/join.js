23574589550903cbabcc25f08d6caf5bafb700cc
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-15 08:14:33 +1000

    Create join.js

---
+++ static/join.js
@@ -0,0 +1,7 @@
+function join(){
+  console.log("joining game");
+  var name = document.getElementById("f_name").value + " " + document.getElementById("l_name").value;
+  var code = document.getElementById("game_code").value;
+  console.log("name: " + name);
+  console.log("code: " + code);
+}
