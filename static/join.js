64da5666823cc67392883f8d98482e5b3dcb072d
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-15 18:16:23 +1000

    Update join.js

--- static/join.js
+++ static/join.js
@@ -2,14 +2,8 @@ function join(){
   var name = document.getElementById("f_name").value + " " + document.getElementById("l_name").value;
   var code = document.getElementById("code").value;
   
-  const data = {
-    "name":name,
-    "code":code
-  }
+  const data = [name,code];
   console.log(data)
-  
-  const test_data_type = [name,code];
-  console.log(test_data_type)
   
   const req = new XMLHttpRequest();
   req.open("POST","/connect2");
@@ -18,14 +12,14 @@ function join(){
       var resp = req.responseText
       if (resp == "[invalid code]"){
         console.log("invalid code")
-      }
-      else if (resp == "[error]"){
-        console.log("An unknown error occured")
-      }
-      else {
-        console.log("game created with code: " + resp)
+      } else if (resp == "[game started]"){
+        console.log("game already started")
+      } else if (resp == "[error]){
+        console.log("an unknown error occured")
+      } else {
+        console.log("user logged with id: " resp)
       }
     }
   }
-  req.send(test_data_type)
+  req.send(data)
 }
