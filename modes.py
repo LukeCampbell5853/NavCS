c2851631788ae8d77103d598693a399138854792
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-16 15:08:48 +1000

    Update modes.py

--- modes.py
+++ modes.py
@@ -4,14 +4,11 @@ from random import randint as r
 
 class HideAndSeek:
   def __init__(self,ip):
-    print("+++RUNNING HideAndSeek __init__+++")
     db = db_man.init_SQL()
     self.code = db_man.get_code(db,ip)
     self.ip = ip
     self.players = {}
-    print(self.code)
     for player in db_man.get_players(db,self.code):
-      print(player)
       data = {}
       data["name"] = player[1]
       data["target"] = player[2]
@@ -19,37 +16,21 @@ class HideAndSeek:
       data["score"] = player[4]
       self.players[player[0]] = data
     db_man.end_query(db)
-    print("---END FUNCTION---")
 
   def assigned(self):
-    print("+++RUNNING HideAndSeek assigned+++")
     result = False
     for player,data in self.players.items():
-      print("printing player [v]")
-      print(player)
-      print(data)
       if data["target"] != "-":
         result = True
         break
-    print("---END FUNCTION---")
     return(result)
 
   def assign_targets(self):
-    print("+++RUNNING HideAndSeek assign_targets+++")
     all_players = list(self.players.keys())
-    print("ALL PLAYERS [v]")
-    print(all_players)
     seeker = all_players[r(0,len(all_players)-1)]
-    print("SEEKER [v]")
-    print(seeker)
     hiders = all_players
     hiders.remove(seeker)
-    print("HIDERS [v]")
-    print(hiders)
     self.players[seeker]["target"] = hiders
     db = db_man.init_SQL()
-    print("updating data [v]")
-    print(self.players[seeker])
     db_man.update_targets(db,seeker,hiders)
     db_man.save(db)
-    print("---END FUNCTION---")
