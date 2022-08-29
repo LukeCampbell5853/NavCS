03db58047f6e7db2e1d5e1c10a52ee47e54b2d29
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-29 15:08:44 +1000

    Update modes.py

--- modes.py
+++ modes.py
@@ -1,30 +1,22 @@
 import database_manager as db_man
+import data_manager as db
 import sqlite3
 from random import randint as r
 from random import shuffle as rs
 
 class HideAndSeek:
-  def __init__(self,ip):
-    db = db_man.init_SQL()
-    self.code = db_man.get_code(db,ip)
-    self.ip = ip
-    self.players = {}
-    for player in db_man.get_players(db,self.code):
-      data = {}
-      data["name"] = player[1]
-      data["target"] = player[2]
-      data["location"] = player[3]
-      data["score"] = player[4]
-      self.players[player[0]] = data
-    db_man.end_query(db)
-
-  def assigned(self):
-    result = False
-    for player,data in self.players.items():
-      if data["target"] != "-":
-        result = True
+  def __init__(self,id):
+    self.me = db.player(id)
+    self.game = db.game(self.me.game)
+    self.assigned = False
+    self.players = []
+    for player in db.players(self.me.game):
+      current_player = db.player(player)
+      self.players.append(current_player)
+    for player in self.players:
+      if player.targets != "":
+        self.assigned = True
         break
-    return(result)
 
   def assign_targets(self):
     all_players = list(self.players.keys())
