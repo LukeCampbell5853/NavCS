4c7eb0f2628f2b63648e81f633e7c471eda0de7c
LukeCampbell5853 <64815494+LukeCampbell5853@users.noreply.github.com>
2022-08-29 14:51:01 +1000

    Update join.js

--- static/join.js
+++ static/join.js
@@ -19,24 +19,24 @@ function join(){
     console.log(data)
 
     const req = new XMLHttpRequest();
-    req.open("POST","/connect2");
+    req.open("POST","/connect");
     const message = document.getElementById("message");
     req.onreadystatechange = function(res){
       if (req.readyState == 4 && req.status == 200){
         var resp = req.responseText;
-        if (resp == "[invalid code]"){
+        if (resp == "1"){
           console.log("invalid code");
           message.innerHTML = "The code you entered didn't match up with any of the games";
-        }
-        else if (resp == "[game started]"){
+        } else if (resp == "2"){
           console.log("game already started");
           message.innerHTML = "The game has already started.";
-        }
-        else if (resp == "[error]"){
+        }else if (resp == "3"){
+          console.log("game already finished");
+          message.innerHTML = "The game has already finished.";
+        }else if (resp == "4"){
           console.log("an unknown error occured");
           message.innerHTML = "An unknown error occured.";
-        }
-        else {
+        }else {
           console.log("user logged with id: " + resp);
           document.cookie = "id:"+resp+";";
           window.location.href = "/run";
